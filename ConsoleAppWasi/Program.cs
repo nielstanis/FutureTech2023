850afa74982719fbe1029a503ca2ff7c20d1715f
Niels Tanis <ntanis@veracode.com>
2023-03-15 08:14:10 +0100

    Initial content

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,4 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello FutureTech 2023 from {RuntimeInformation.OSArchitecture}!");
---
+++ Extend/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Hello, FutureTech 2023!");
+
+Directory.CreateDirectory("log");
+Wasmtime.WasiConfiguration conf = new Wasmtime.WasiConfiguration()
+    .WithPreopenedDirectory("/etc","/etc")
+    .WithPreopenedDirectory(".",".")
+    .WithStandardError("log/error.log")
+    .WithStandardOutput("log/output.log")
+    .WithArg("CalledFromConsole")
+    .WithArgs(args); //passing args
+
+var engineConfig = new Wasmtime.Config();
+//engineConfig.WithFuelConsumption(true);
+var engine = new Wasmtime.Engine(engineConfig);
+var linker = new Wasmtime.Linker(engine);
+linker.DefineWasi();
+var store = new Wasmtime.Store(engine);
+store.SetWasiConfiguration(conf);
+//store.AddFuel(5000);
+
+string wasm = @"wasm/myrustmodule.wasm";
+var module = Wasmtime.Module.FromFile(engine, wasm);
+var inst = linker.Instantiate(store, module);
+var main = inst.GetFunction("main");
+
+if (main!=null)
+    main.Invoke(0,0);
+
+//Console.WriteLine($"Consumed fuel: {store.GetConsumedFuel()}");
